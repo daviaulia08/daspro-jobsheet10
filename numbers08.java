9f9ec702a162de8cc5aaa6c2de74b9de453b15ff
Your Name <you@example.com>
2024-11-07 05:48:36 +0700

    modifikasi no 4

--- numbers08.java
+++ numbers08.java
@@ -14,5 +14,9 @@ public class numbers08 {
         for (int i = 0; i < myNumbers.length; i++) {
             System.out.println(Arrays.toString(myNumbers[i]));
         }
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);
+        }
     }
 }
