8babc9d4e167c1b28b75cf00876d91c3886e5a36
Your Name <you@example.com>
2024-11-07 05:42:45 +0700

    modifikasi no 1

--- numbers08.java
+++ numbers08.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class numbers08 {
     public static void main (String[] args) {
@@ -9,5 +10,9 @@ public class numbers08 {
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
     }
 }
